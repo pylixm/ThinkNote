57c16dc81d58db3a279644478d63922ef2667b9b
pylixm <pyli.xm@gmail.com>
2018-10-17 10:19:59 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-echo '>>>>>>> 开始生成html文件'
+echo -e '\033[32m >>>>>>> 开始生成html文件 \033[0m'
 hexo g 
-echo '>>>>>>> 开始提交静态文件'
+echo -e '\033[32m >>>>>>> 开始提交静态文件 \033[0m'
 cd public 
 git add .
 git commit -am 'update'
 git push 
-echo '>>>>>>> 主库文件受控更新'
+echo -e '\033[32m >>>>>>> 主库文件受控更新  \033[0m'
 cd ..
 git add .
 git commit -am 'update'
 git push 
-echo '>>>>>>> 发布完成！！！ <<<<<<<< '
+echo '\033[32m >>>>>>> 发布完成！！！ <<<<<<<<  \033[0m'
