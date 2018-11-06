eddbc73b4ad9b15c67c156b9a9a81fe52db4ac53
DeanWu <pyli.xm@gmail.com>
2018-11-06 10:13:58 +0800

    $1

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,12 @@ echo '\033[31m >>>>>>> 开始提交静态文件 \033[0m'
 cp -r ./public/* ./deploy/
 cd deploy
 git add .
-git commit -am 'update'
+git commit -am '$1'
 git push 
 echo '\033[31m >>>>>>> 静态文件提交完成 \033[0m'
 echo '\033[31m >>>>>>> 主库文件受控更新  \033[0m'
 cd ..
 git add .
-git commit -am 'update'
+git commit -am '$1'
 git push 
 echo '\033[31m >>>>>>> 发布完成！！！ <<<<<<<<  \033[0m'
